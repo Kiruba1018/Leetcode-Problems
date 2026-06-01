18a45ea60effcc0828e53ca20c03ec42ecb3750f
Kirubagaran M <146163145+Kiruba1018@users.noreply.github.com>
2026-06-01 13:09:12 +0530

    Time: 14 ms (85.01%) | Memory: 71.4 MB (92.99%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+   let s = new Set();
+   for(let i = 0; i < nums.length; i++){
+    if(s.has(nums[i])){
+        return true
+    }
+    s.add(nums[i])
+   }
+   return false
+   
+};
